585ff405e1f29e0f93336023b0862307f8234757
Sam Craig <sammidysam@gmail.com>
2015-01-07 17:13:29 -0500

    Add nav_id to Page

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107213515) do
+ActiveRecord::Schema.define(version: 20150107221125) do
 
   create_table "pages", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "nav_id"
   end
 
 end
