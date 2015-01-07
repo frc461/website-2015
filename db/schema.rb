8dcea5a96bd12516a3e4517ea646fed841194ec5
Bencourt <lincourtbenjamin@gmail.com>
2015-01-07 18:21:30 -0500

    I deserve a second gold star

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107221125) do
+ActiveRecord::Schema.define(version: 20150107221929) do
+
+  create_table "navs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "sort_index"
+    t.string   "link"
+    t.integer  "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string   "name"
