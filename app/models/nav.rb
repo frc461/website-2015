d7bb3e414c9aca72d1d9bdd294ab83d0e1c88d99
Sammidysam <sammidysam@gmail.com>
2015-01-08 16:52:21 -0500

    Modify how models look

--- app/models/nav.rb
+++ app/models/nav.rb
@@ -1,5 +1,6 @@
 class Nav < ActiveRecord::Base
-	has_one :page
 	belongs_to :parent, class_name: "Nav"
+	
+	has_one :page
 	has_many :children, class_name: "Nav", foreign_key: :parent_id
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,6 @@
 class News < ActiveRecord::Base
 	belongs_to :creator, class_name: "User", foreign_key: "creator_dn"
+	
 	has_one :photo
 	has_one :page
 end
