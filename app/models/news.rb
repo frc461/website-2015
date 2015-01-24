d7d0864d3effba361874b6213e7c461b0855ac80
BColliesRSmart <bordercolliesaresmart@gmail.com>
2015-01-24 14:29:28 -0500

    Changed Login Hover Color

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,8 @@
 class News < ActiveRecord::Base
-	belongs_to :creator, class_name: "User", foreign_key: "creator_dn"
-	
+
 	has_one :photo
 	has_one :page
+	def user
+	    User.find(creator_dn)
+        end
 end
