da6d57ea8c49c8e64000d6dc674280f82f2f46f5
releanor <releanor9@gmail.com>
2015-06-01 20:00:16 -0400

    Adding useful information in preparation script

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,8 @@
+echo "Copying files..."
 cp config/database.yml.sample config/database.yml
 cp config/secrets.yml.sample config/secrets.yml
 
+echo "Migrating the database!"
 rake db:migrate
 
 echo "Project should now be ready to roll!"
